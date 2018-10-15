24d690f90e3e1117683f8fd89e12f42fcfc9b099
lijianzhq <804390150@qq.com>
2018-10-15 14:03:44 +0800

    新增netcore test web

---
+++ trunk/web/TestForAll/test1.js
@@ -0,0 +1,20 @@
+﻿
+//var url = 'https://hq.tigerbrokers.com/fundamental/finance_calendar/getType/2017-02-26/2017-06-10';
+//var result;
+
+//var XHR = new XMLHttpRequest();
+//XHR.open('GET', url, true);
+//XHR.send();
+
+//XHR.onreadystatechange = function () {
+//    if (XHR.readyState == 4 && XHR.status == 200) {
+//        result = XHR.response;
+//        console.log(result);
+//    }
+//}
+
+
+function test() {    console.log(foo);    console.log(bar);
+    var foo = 'Hello';    console.log(foo);    var bar = function () {        return 'world';    }
+    function foo() {        return 'hello';    }}
+test();
---
+++ trunk/web/TestForAll/testJSModule/lib.js
@@ -0,0 +1,8 @@
+﻿export const sqrt = Math.sqrt;
+export function square(x) {
+    return x * x;
+}
+export function diag(x, y) {
+    return sqrt(square(x) + square(y));
+}
+
